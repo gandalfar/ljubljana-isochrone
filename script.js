581f31af1d587525ee95e956170a8afa3eaba36e
Jure Cuhalev <gandalfar@gmail.com>
2016-07-02 21:49:13 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,79 @@
+jQuery(document).ready(function($) {
+	var map, googleLayer, maskLayer, marker, slider, points;
+
+	updateMap = function(){
+		var sliderValue = Math.round(slider.noUiSlider.get()) * 60;
+
+		if ( points ) {
+			var visiblePoints = $.grep(points, function(item, i) {
+				return sliderValue >= item[2];
+			});
+
+			maskLayer.setData(visiblePoints);
+			map.addLayer(maskLayer);
+
+			$('.js-travel-time').html(Math.round(sliderValue / 60) + ' minutes');
+		} else {
+			$.get('data.json', function(data) {
+				points = data;
+				updateMap();
+			});
+		}
+	};
+
+	slider = $('#slider').get(0);
+	noUiSlider.create(slider, {
+		start: [0],
+		step: 5,
+		range: {
+			'min': 0,
+			'max': 120
+		},
+		connect: 'lower',
+		pips: { 
+			mode: 'steps',
+			density: 2
+		}
+
+	});
+	slider.noUiSlider.on('change', updateMap);
+
+ // map = L.map('map').setView([46.056515, 14.501460], 11);
+ //    var osm = L.tileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
+ //        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+ //        maxZoom: 15
+ //    }).addTo(map);
+
+	map = L.map('map').setView([46.056515, 14.501460], 11);
+	googleLayer = new L.Google('ROADMAP', {
+			options: {
+				maxZoom: 13
+			}
+	});
+	map.addLayer(googleLayer);
+	
+	maskLayer = L.TileLayer.maskCanvas({
+		radius: 800,
+		useAbsoluteRadius: true
+	});
+	marker = L.marker([46.048148, 14.493625]).addTo(map);
+	updateMap();
+
+	$(window).on("resize", function() {
+	    $("#map").height( $(window).height() - $('.container-main').height() );
+	    map.invalidateSize();
+	}).trigger("resize");
+
+	slider.noUiSlider.set(0); updateMap();
+	window.setTimeout(function(){ slider.noUiSlider.set(5);  updateMap(); }, 1000  *   1);
+	window.setTimeout(function(){ slider.noUiSlider.set(10); updateMap(); }, 1000  *   2);
+	window.setTimeout(function(){ slider.noUiSlider.set(15); updateMap(); }, 1000  *   3);
+	window.setTimeout(function(){ slider.noUiSlider.set(20); updateMap(); }, 1000 *    4);
+	window.setTimeout(function(){ slider.noUiSlider.set(25); updateMap(); }, 1000  *   5);
+	window.setTimeout(function(){ slider.noUiSlider.set(30); updateMap(); }, 1000  *   6);
+	window.setTimeout(function(){ slider.noUiSlider.set(35); updateMap(); }, 1000 *    7);
+	window.setTimeout(function(){ slider.noUiSlider.set(40); updateMap(); }, 1000  *   8);
+	window.setTimeout(function(){ slider.noUiSlider.set(45); updateMap(); }, 1000  *   9);
+	window.setTimeout(function(){ slider.noUiSlider.set(45); updateMap(); }, 1000  *  10);
+
+});
